54fb753d9cc9efdea4b6f35e86791cad6546d49f
Mshafran <Mshafran@stuy.edu>
2017-01-12 19:33:29 -0500

    Add files via upload

---
+++ BlackjackPlayer.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class BlackjackPlayer extends Player{
+    public ArrayList<Card> hand= new ArrayList<Card>();
+    public boolean myTurn = false;
+
+    
+    
+}
