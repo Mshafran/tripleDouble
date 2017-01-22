b3455ed5d15fb88b6a3eb394a5b05f4cb3576c3f
Mark Shafran <Mark Shafran>
2017-01-22 12:25:06 -0500

    Added comments to WarAI and WarUser

--- WarAI.java
+++ WarAI.java
@@ -1,27 +1,23 @@
 import java.util.ArrayList;
-public class WarAI extends Player{
-    public ArrayList<Card> table = new ArrayList<Card>();
+public class WarAI extends Player{ // subclass of Player
+    public ArrayList<Card> table = new ArrayList<Card>(); // has an ArrayList called table, which has all the cards it plays
 
+
+    public void move() { // move method for AI
+	putCard(); // puts a card on the table
+	}
     
     public void putCard(){
-	this.table.add(this.hand.remove(0));
+	this.table.add(this.hand.remove(0));// moves a card from the hand to the deck
     }
 
-public void tie() {
-    putCard();
-    putCard();
+    public void tie() {// in case of a tie
+	putCard();//puts down "mystery" card
+	putCard();// puts down card that is compared for the War
+    }
 }
 
-    
-    public void move() {
-	putCard();
-	}
-
-    /*public static void main(String[] args) {
-	move();
-	}*/
 		
 
     
-    
-}
+
--- WarUser.java
+++ WarUser.java
@@ -3,45 +3,34 @@ import cs1.Keyboard;
 
 public class WarUser extends Player{
     public ArrayList<Card> table = new ArrayList<Card>();
-    public boolean Tie = false;
 
-    
-    public void putCard(){
-	this.table.add(this.hand.remove(0));
-    }
-
-public void tie() {
-    putCard();
-    putCard();
-}
-
-    
-    public void move() {
-	War.printInstructions();
-	System.out.println("This is how many cards YOU currently have: " + hand.size());
-	System.out.println("This is how many cards YOUR OPPONENT currently has: " + (52 - hand.size()));
+    public void move() {// one turn for the user
+	War.printInstructions(); // prints instructions from War
+	System.out.println("This is how many cards YOU currently have: " + hand.size()); // prints out how many cards user has
+	System.out.println("This is how many cards YOUR OPPONENT currently has: " + (52 - hand.size())); // prints out how many cards the AI has
 	boolean turn = true;
-	while ((turn == true) && (hand.size() < 52) && (hand.size() > 0)) {
-	    String input = Keyboard.readWord();
-	    if (input.equals("go")) {
-	        putCard();
-		turn = false;
-	    } else if (input.equals("forfeit")) {
-		turn = false;
-		Woo.main(null);
-	    } else if (input.equals("help")) {
-	        War.printInstructions();
-	    } else {
-		System.out.println("Sorry, but what?");
+	while ((turn == true) && (hand.size() < 52) && (hand.size() > 0)) {// runs while it is still the users turn AND the user has not won or lost yet
+	    String input = Keyboard.readWord();// what the user typed in
+	    if (input.equals("go")) { // if user typed in go
+	        putCard(); // put card down
+		turn = false; // end turn
+	    } else if (input.equals("forfeit")) {//if user typed in forfeit
+		turn = false; // end turn
+		Woo.main(null); // return to game selection
+	    } else if (input.equals("help")) { // if the user typed in help
+	        War.printRules();// printRules method from War
+	    } else { // anything else
+		System.out.println("Sorry, but what?"); // asks again
 	    }
 	}
     }
-
-    /*public static void main(String[] args) {
-	move();
-	}*/
-		
-
     
-    
+    public void putCard(){ // puts down card
+	this.table.add(this.hand.remove(0));// moves card from hand to table
+    }
+
+    public void tie() {// in case of a tie
+	putCard();// puts down "mystery card"
+	putCard();// puts down card to compare
+    }
 }
